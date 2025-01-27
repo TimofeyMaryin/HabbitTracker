4d8c87ff1c034a01c7ec580bf442c3da11bb4f48
Ілона Мельник <66883153+TimofeyMaryin@users.noreply.github.com>
2025-01-27 22:27:50 +0500

    Update Habit Entity: add fields

--- domain/src/main/java/com/broad/habbit/domain/entity/Habit.kt
+++ domain/src/main/java/com/broad/habbit/domain/entity/Habit.kt
@@ -5,34 +5,44 @@ interface HabitBuilder {
     fun setName(name: String): HabitBuilder
     fun setDescription(description: String): HabitBuilder
     fun setCategory(category: HabitCategory): HabitBuilder
+    fun setHabitInitiation(initiation: String): HabitBuilder
+    fun setHabitFrequency(frequency: HabitFrequency): HabitBuilder
 }
 
 data class Habit(
     var name: String,
     var description: String,
     var category: HabitCategory,
+    var habitInitiation: String,
+    var frequency: HabitFrequency,
 ) {
     class Builder: HabitBuilder {
         private var name: String = ""
         private var description: String = ""
+        private var habitInitiation: String = ""
         private var category: HabitCategory = HabitCategory.NONE
+        private var frequency: HabitFrequency = HabitFrequency.NONE
 
         override fun setName(name: String) = apply { this.name = name }
         override fun setDescription(description: String) = apply { this.description = description }
         override fun setCategory(category: HabitCategory) = apply { this.category = category }
+        override fun setHabitInitiation(initiation: String): HabitBuilder = apply { this.habitInitiation = initiation }
+        override fun setHabitFrequency(frequency: HabitFrequency): HabitBuilder = apply { this.frequency = frequency }
 
         fun build(): Habit {
             return Habit(
                 name = this.name,
                 description = this.description,
-                category = this.category
+                category = this.category,
+                habitInitiation = this.habitInitiation,
+                frequency = this.frequency
             )
         }
     }
 }
 
 
-enum class HabitCategory {
-    LESSON, STUDY, WORK, NONE
-}
+enum class HabitCategory { LESSON, STUDY, WORK, NONE }
+enum class HabitFrequency { DAILY, WEEKLY, MONTHLY, NONE }
+
 
